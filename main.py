eef29f693c0ff3885c385110dbfcdf3f0d44dd2e
rchrd <rchrd.pdrz.mrtnz@gmail.com>
2020-08-11 22:07:56 -0400

    main changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import random
 from wordrandom import wordrand
@@ -6,24 +7,33 @@ from game import game
 from title import title
 
 title()
-start = input(
-    "Only silly children become hanged, Now press enter to dead \n".center(140))
+os.system(
+    'read -s -n 1 -p "\nOnly silly children become hanged, now press any key \n"')
 time.sleep(0.2)
 try:
     level = int(input(
-        "Now is the time to prove that you are not a fool, choose the level from 1 to 3".center(140)))
+        "Now is the time to prove that you are not a fool, choose the level from 1 to 3:  "))
 except:
-    print("only 1 or 2 or 3\n".center(140))
+    print("only 1 or 2 or 3 \n".center(140))
     level = int(
-        input(" you are a fool, choose again the level from 1 or 2 or 3 \n".center(140)))
+        input(" you are a fool, choose again the level from 1 or 2 or 3 \n"))
 lifes = difficulty(level)
 
 game(lifes)
 
-goback = input("Do you want to try again useless? Y/N")
-while goback == "y":
-    title()
-    vidas = 10
-    game(lifes)
-    goback = input("Do you want to try again useless? Y/N \n")
+goback = input("\nDo you want to try again useless? Y/N\n")
 
+if goback == "y" or goback == "Y" or goback == "yes" or goback=="YES":
+    while goback == "y" or "Y" or "yes":
+        title()
+        vidas = 10
+        game(lifes)
+        goback = input("Do you want to try again useless? Y/N \n")
+        if goback == "y" or goback == "Y" or goback == "yes" or goback=="YES":
+            pass
+        elif goback == "n" or goback == "N" or goback == "not" or goback == "NOT":
+            break
+
+elif goback == "n" or goback == "N" or goback == "not" or goback == "NOT":
+    print("are you a chicken?\nThis is not for cowards")
+    time.sleep(0.5)
---
+++ prueba.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('read -s -n 1 -p "Only silly children become hanged, Now press enter to dead "')
+suma=5+4
+print(suma)
